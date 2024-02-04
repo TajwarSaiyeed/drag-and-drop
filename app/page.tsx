2319681463eabc95ad8af00dbde626d627a253af
tajwarsaiyeed <rjabid36@gmail.com>
2024-02-04 18:33:53 +0600

    chore: removed unused files

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Hero from "@/components/Hero/Hero";
 import React from "react";
 
 const HomePage = () => {
-  return (
-    <div>
-      <Hero />
-    </div>
-  );
+    return (
+        <div>
+            <Hero/>
+        </div>
+    );
 };
+
+export const dynamic = 'force-dynamic';
 
 export default HomePage;
