0e854d5ae826926ca7ba23d849b6ea6b12878ce1
taiyebnirjhar <nirjhar.212902003@green.ac.bd>
2024-02-04 16:15:04 +0600

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
+import { mergePDFs } from "@/helpers/mergePDFs";
 import useScreenSize from "@/hooks/useScreenSize";
 import { cn } from "@/lib/utils";
 import {
@@ -10,7 +11,7 @@ import {
   Droppable,
 } from "@hello-pangea/dnd";
 import workerSrc from "pdfjs-dist/build/pdf.worker.entry.js";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Document, Page, pdfjs } from "react-pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = workerSrc;
 
@@ -21,19 +22,6 @@ function reorder<T>(item: T[], startIndex: number, endIndex: number) {
 
   return result;
 }
-
-// const mergePDFs = (pdfFiles: any[]) => {
-//   const mergedPdf = new jsPDF();
-
-//   pdfFiles.forEach((pdfFile) => {
-//     const pdfData = new Uint8Array(pdfFile.file);
-//     const pdf = new jsPDF();
-//     pdf.loadDocument(pdfData);
-//     mergedPdf.addDocument(pdf);
-//   });
-
-//   return mergedPdf;
-// };
 
 const HomePage = () => {
   const [pageNumber, setPageNumber] = useState<number>(1);
@@ -69,6 +57,10 @@ const HomePage = () => {
     }));
   };
 
+  const handleButtonClick = () => {
+    const fileInput = document.getElementById("file-input") as HTMLInputElement;
+    fileInput.click();
+  };
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
     if (!files) return;
@@ -81,31 +73,61 @@ const HomePage = () => {
     setItems([...items, ...newItems]);
   };
 
-  const handleButtonClick = () => {
-    const fileInput = document.getElementById("file-input") as HTMLInputElement;
-    fileInput.click();
+  const handleMergePDFs = async () => {
+    if (items && items.length > 0) {
+      try {
+        const pdfBuffers = await Promise.all(
+          items.map(async (item) => {
+            const arrayBuffer = await item.file.arrayBuffer();
+            return new Uint8Array(arrayBuffer);
+          })
+        );
+
+        const mergedPDFBuffer = await mergePDFs(pdfBuffers);
+
+        const blob = new Blob([mergedPDFBuffer], { type: "application/pdf" });
+        const link = document.createElement("a");
+        link.href = window.URL.createObjectURL(blob);
+        link.download = "merged.pdf";
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+      } catch (error) {
+        console.error("Error merging PDFs:", error);
+        // Handle the error as needed
+      }
+    } else {
+      alert("Add Some Pdf");
+    }
   };
 
   if (!isMounted) return null;
 
   return (
-    <div className={"relative"}>
-      <Button
-        onClick={handleButtonClick}
-        variant={"destructive"}
-        size={"lg"}
-        className={cn("", items.length > 0 && "absolute top-0 right-0 z-10")}
-      >
-        <input
-          type="file"
-          accept="application/pdf"
-          multiple
-          id="file-input"
-          onChange={handleFileChange}
-          className={"hidden"}
-        />
-        Add PDF
-      </Button>
+    <div className={"relative "}>
+      {items.length === 0 && (
+        <React.Fragment>
+          <Button
+            onClick={handleButtonClick}
+            variant={"destructive"}
+            size={"lg"}
+            className={cn(
+              "",
+              items.length > 0 && "absolute top-0 right-0 z-10"
+            )}
+          >
+            <input
+              type="file"
+              accept="application/pdf"
+              multiple
+              id="file-input"
+              onChange={handleFileChange}
+              className={"hidden"}
+            />
+            Add PDF
+          </Button>
+        </React.Fragment>
+      )}
 
       <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId={"items"} direction={"horizontal"} type={"list"}>
@@ -149,6 +171,18 @@ const HomePage = () => {
           )}
         </Droppable>
       </DragDropContext>
+
+      <div className="mt-24">
+        <Button
+          variant={"destructive"}
+          size={"lg"}
+          className={cn("", items.length > 0 && "absolute top-0 right-0 z-10")}
+          type="button"
+          onClick={handleMergePDFs}
+        >
+          Merge Pdf
+        </Button>
+      </div>
     </div>
   );
 };
---
+++ helpers/mergePDFs.ts
@@ -0,0 +1,15 @@
+// yourPdfLibModule.js
+import { PDFDocument } from "pdf-lib";
+
+export const mergePDFs = async (pdfBuffers) => {
+  console.log(pdfBuffers);
+  const mergedPDF = await PDFDocument.create();
+
+  for (const pdfBuffer of pdfBuffers) {
+    const pdf = await PDFDocument.load(pdfBuffer);
+    const pages = await mergedPDF.copyPages(pdf, pdf.getPageIndices());
+    pages.forEach((page) => mergedPDF.addPage(page));
+  }
+
+  return await mergedPDF.save();
+};
